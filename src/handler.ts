66bfae69cfb61246e8f58705a19d3124e44146dd
cormojs <cormorant2+github@gmail.com>
2019-11-07 07:39:36 +0900

    Modify handler.ts

--- src/handler.ts
+++ src/handler.ts
@@ -1,27 +1,21 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
 import 'source-map-support/register'
-import _ from 'lodash'
-import nijie from './metascraper-nijie'
+import * as got from 'got'
+import { melonbooks } from './resolver/melonbooks-resolver'
+import { defaultResolver } from './resolver/default-resolver'
+import { nijie } from './resolver/nijie-resolver'
+import { buildResolver } from './resolver/resolver'
+import * as metascraper from 'metascraper'
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  const metascraper = require('metascraper')([
-    nijie(),
-    require('metascraper-author')(),
-    require('metascraper-date')(),
-    require('metascraper-description')(),
-    require('metascraper-image')(),
-    require('metascraper-logo')(),
-    require('metascraper-publisher')(),
-    require('metascraper-title')(),
-    require('metascraper-url')()
-  ])
-
-  const got = require("got")
-  const targetUrl = 'http://nijie.info/view.php?id=55726'
-  const { body: html, url } = await got(targetUrl)
-  const metadata = await metascraper({ html, url })
+  const resolver = buildResolver([ melonbooks, nijie, defaultResolver ])
+  console.log(event)
+  const targetUrl = 'http://nijie.info/view.php?id=339139'
+  const resolved = resolver(targetUrl)
+  const { body: html, url } = await got(resolved.url(), resolved.options())
+  const metadata = await metascraper([resolved.rule()])({ html, url })
   return {
     statusCode: 200,
-    body: JSON.stringify(metadata, null),
+    body: JSON.stringify(metadata)
   };
 }
